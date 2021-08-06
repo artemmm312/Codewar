4ecb5704ed20deafec4fcc2a2fae6d7d80bade60
artemmm312 <artemmm312@gmail.com>
2021-08-06 12:49:39 +0300

    fix

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
-//Bit Counting 
+//Are they the "same"?
 
-function countBits($n) 
+function comp($a1, $a2)
 {
-	if ($n > 0)
+	if($a1 > 0 && $a2 > 0 && count($a1) == count($a2))
 	{
-	$b = decbin($n);
-	return substr_count ($b, "1");
+		sort($a1);
+		sort($a2);
+		$arr = [];
+		for ($i = 0, $j = 0, $size1 = count($a1), $size2 = count($a2); $i < $size1, $j < $size2; $i++, $j++)
+		{
+			if ($a1[$i]**2 == $a2[$j]) $arr[] = 'true';
+			else $arr[] = 'false';
+		}
+		if (in_array('false', $arr)) return false;
+		else return true;
 	}
-	else echo "Number must be whole and non-negative";
+	else return false;
 }
-print_r(countBits(113));
