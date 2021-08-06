34ebd38dd860eb7286f77e78f59d7f3a39f86fab
artemmm312 <artemmm312@gmail.com>
2021-08-06 12:53:15 +0300

    fix

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
-//Are they the "same"?
+//Sum of the first nth term of Series
 
-function comp($a1, $a2)
+function series_sum($n)
 {
-	if($a1 > 0 && $a2 > 0 && count($a1) == count($a2))
-	{
-		sort($a1);
-		sort($a2);
-		$arr = [];
-		for ($i = 0, $j = 0, $size1 = count($a1), $size2 = count($a2); $i < $size1, $j < $size2; $i++, $j++)
-		{
-			if ($a1[$i]**2 == $a2[$j]) $arr[] = 'true';
-			else $arr[] = 'false';
-		}
-		if (in_array('false', $arr)) return false;
-		else return true;
+	$sum = 0;
+	for ($i = 0, $j = 1; $i < $n; $i++, $j += 3) {
+		$sum += (1 / $j);
 	}
-	else return false;
+	$sum = number_format($sum, 2, ".");
+	return "$sum";
 }
+
+print_r(series_sum(5));
