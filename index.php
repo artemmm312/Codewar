e5bc2c646114e4b79a700b8d1d97bc1588abdc63
artemmm312 <artemmm312@gmail.com>
2021-08-06 14:37:41 +0300

    fix

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php
-//Build Tower
+//Roman Numerals Decoder
 
-function tower_builder(int $n): array
+function solution($roman)
 {
-	if ($n > 0) {
-		$tower = [];
-		$block = "*";
-		$length = $n * 2 - 1;
-		for ($i = 1, $j = 1; $i <= $n, $j <= $length; $i++, $j += 2) {
-			$floor = str_pad(str_pad($block, $j, "*"), $length, " ", STR_PAD_BOTH);
-			$tower[] = $floor;
-		}
-		return $tower;
-	} else echo "The number of floors must be integer and greater than zero";
-}
+	$number = 0;
 
-print_r(tower_builder(5));
+	$romNum = [
+		'I' => 1,
+		'V' => 5,
+		'X' => 10,
+		'L' => 50,
+		'C' => 100,
+		'D' => 500,
+		'M' => 1000
+	];
+
+	$rom = str_split($roman, 1);
+	$count = count($rom);
+	for ($i = 0, $j = 1; $i < $count; $i++, $j++) {
+		if ($j < $count) {
+			if ($romNum[$rom[$i]] >= $romNum[$rom[$j]]) $number += $romNum[$rom[$i]];
+			else {
+				$number += $romNum[$rom[$j]] - $romNum[$rom[$i]];
+				$i++;
+				$j++;
+			}
+		} elseif ($j == $count && $count > 1) {
+			if ($romNum[$rom[$i - 1]] >= $romNum[$rom[$i]]) $number += $romNum[$rom[$i]];
+			else break;
+		} else $number += $romNum[$rom[$i]];
+	}
+	return $number;
+}
+print_r(solution($roman));
